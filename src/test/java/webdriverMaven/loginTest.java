d923e1774a1f2e8187d0c23fa5ddc29d90de32e4
swatiagl0405 <swati.iet@gmail.com>
2016-02-12 12:35:09 +0530

    Committing the changes made to loginTest

--- src/test/java/webdriverMaven/loginTest.java
+++ src/test/java/webdriverMaven/loginTest.java
@@ -1,5 +1,7 @@
 package webdriverMaven;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,10 +13,12 @@ public class loginTest {
 
 	public static WebDriver driver;
 	
+	
 	@BeforeSuite
 	public void setUp(){
 		
 		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20L, TimeUnit.SECONDS);
 		
 	}
 	
@@ -23,7 +27,9 @@ public class loginTest {
 		
 		driver.get("http://gmail.com");
 		driver.findElement(By.id("Email")).sendKeys("swati.iet@gmail.com");
-		//driver.findElement(By.id("Passwd")).sendKeys("123");
+		driver.findElement(By.id("next")).click();
+		driver.findElement(By.id("Passwd")).sendKeys("snaponswat123");
+		driver.findElement(By.id("signIn")).click();
 	}
 	
 	@AfterSuite
