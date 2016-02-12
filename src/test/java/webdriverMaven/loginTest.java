937e73f75163f4a8845687e9c2d1024ab56bd554
swatiagl0405 <swati.iet@gmail.com>
2016-02-12 06:53:39 +0530

    Initial commit

---
+++ src/test/java/webdriverMaven/loginTest.java
@@ -0,0 +1,34 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class loginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		
+		driver = new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void doLogin(){
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("swati.iet@gmail.com");
+		//driver.findElement(By.id("Passwd")).sendKeys("123");
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		
+		driver.quit();
+	}
+}
